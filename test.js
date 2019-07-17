50b7261377cfd9c09c4e1fad810029339b819356
khushalkunjir <43520593+khushalkunjir@users.noreply.github.com>
2019-07-17 21:04:22 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,32 @@
+const should = require("should");
+const request = require("request");
+const expect = require("chai").expect;
+const baseUrl = "https://swapi.co/api";
+const util = require("util");
+
+describe('returns luke', function() {
+    it('returns luke', function(done) {
+        request.get({ url: baseUrl + '/people/1/' },
+            function(error, response, body) {
+            		const bodyObj = JSON.parse(body);
+            		expect(bodyObj.name).to.equal("Luke Skywalker");
+            		expect(bodyObj.hair_color).to.equal("blond");
+                    expect(response.statusCode).to.equal(200);
+                    console.log(body);
+                done();
+            });
+    });
+
+    it('should return string',function(done){
+            request.get({url:baseUrl+'/people/1/'},
+            function(error,response,body){
+                    const bodyObj1=JSON.parse(body);
+                    
+            
+            expect(bodyObj1.name).to.be.a('string');
+            expect(response.statusCode).to.equal(200);
+            done();
+            });
+    }) 
+
+});
